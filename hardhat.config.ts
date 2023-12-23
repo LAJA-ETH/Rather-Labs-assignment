442709a76b9adbed7dd93499cdb52007c4fea312
LAJA-ETH <omolajatomi904@gmail.com>
2023-12-23 14:01:08 -0500

    Your commit message here

---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@typechain/hardhat'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
+import './tasks'
+import { forkingBlockNumber, mainNetRpcUrl } from './env'
+
+const config: HardhatUserConfig = {
+  namedAccounts: {
+    owner: 0,
+    userA: 1,
+    userB: 2,
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: mainNetRpcUrl,
+        blockNumber: Number(forkingBlockNumber),
+        enabled: true,
+      },
+    },
+    localhost: {
+      chainId: 31337,
+      timeout: 2000000,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.13',
+      },
+      { version: '0.6.12' },
+    ],
+  },
+  typechain: {
+    outDir: 'typechain-types',
+    target: 'ethers-v5',
+  },
+}
+
+export default config
